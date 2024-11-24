d6c4bb9c176d8dc0da256704910bc3b6b4164de0
Owusu-Piadu <eugeneowusupiadu@gmail.com>
2024-11-24 22:55:24 +0100

    new file:   safebox/app/(auth)/layout.tsx 	new file:   safebox/app/(auth)/sign-in/page.tsx 	new file:   safebox/app/(auth)/sign-up/page.tsx 	modified:   safebox/app/favicon.ico 	modified:   safebox/app/globals.css 	modified:   safebox/app/page.tsx 	modified:   safebox/package-lock.json 	modified:   safebox/package.json 	new file:   safebox/public/assets/icons/arrow-up.svg 	new file:   safebox/public/assets/icons/calendar.svg 	new file:   safebox/public/assets/icons/close-dark.svg 	new file:   safebox/public/assets/icons/close.svg 	new file:   safebox/public/assets/icons/dashboard.svg 	new file:   safebox/public/assets/icons/delete.svg 	new file:   safebox/public/assets/icons/documents.svg 	new file:   safebox/public/assets/icons/dots.svg 	new file:   safebox/public/assets/icons/download.svg 	new file:   safebox/public/assets/icons/edit.svg 	new file:   safebox/public/assets/icons/file-audio.svg 	new file:   safebox/public/assets/icons/file-csv.svg 	new file:   safebox/public/assets/icons/file-doc.svg 	new file:   safebox/public/assets/icons/file-document-light.svg 	new file:   safebox/public/assets/icons/file-document.svg 	new file:   safebox/public/assets/icons/file-docx.svg 	new file:   safebox/public/assets/icons/file-image-light.svg 	new file:   safebox/public/assets/icons/file-image.svg 	new file:   safebox/public/assets/icons/file-loader.gif 	new file:   safebox/public/assets/icons/file-other-light.svg 	new file:   safebox/public/assets/icons/file-other.svg 	new file:   safebox/public/assets/icons/file-pdf.svg 	new file:   safebox/public/assets/icons/file-svg.svg 	new file:   safebox/public/assets/icons/file-txt.svg 	new file:   safebox/public/assets/icons/file-video-light.svg 	new file:   safebox/public/assets/icons/file-video.svg 	new file:   safebox/public/assets/icons/grid.svg 	new file:   safebox/public/assets/icons/images.svg 	new file:   safebox/public/assets/icons/info.svg 	new file:   safebox/public/assets/icons/loader-brand.svg 	new file:   safebox/public/assets/icons/loader.svg 	new file:   safebox/public/assets/icons/logo-brand.svg 	new file:   safebox/public/assets/icons/logo-full-brand.svg 	new file:   safebox/public/assets/icons/logo-full.svg 	new file:   safebox/public/assets/icons/logout.svg 	new file:   safebox/public/assets/icons/menu.svg 	new file:   safebox/public/assets/icons/others.svg 	new file:   safebox/public/assets/icons/remove.svg 	new file:   safebox/public/assets/icons/search.svg 	new file:   safebox/public/assets/icons/share.svg 	new file:   safebox/public/assets/icons/thumb-other.svg 	new file:   safebox/public/assets/icons/thumb-video.svg 	new file:   safebox/public/assets/icons/upload.svg 	new file:   safebox/public/assets/icons/video.svg 	new file:   safebox/public/assets/images/avatar.png 	new file:   safebox/public/assets/images/files-2.png 	new file:   safebox/public/assets/images/files.png 	new file:   safebox/public/assets/images/photo.png 	modified:   safebox/tailwind.config.ts

--- safebox/app/page.tsx
+++ safebox/app/page.tsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div >
-      
+      <h1 className="text-3l text-brand">
+        SafeBox - Drop it in a Safe Place.
+      </h1> 
     </div>
   );
 }
